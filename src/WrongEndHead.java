03064758eb3eefdc0b27d05838ff9cc60e90eed6
Maksim_Korelov <Korelov@gmail.com>
2021-09-24 14:06:12 +0300

    Array reverse

---
+++ src/WrongEndHead.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+//https://www.codewars.com/kata/56f699cd9400f5b7d8000b55/train/java
+public class WrongEndHead {
+    public static String[] fixTheMeerkat(String[] arr) {
+        String[] b = new String[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            b[i] = arr[arr.length - 1 - i];
+        }
+        return b;
+    }
+        public static void main(String[] args) {
+
+            String[] a = new String[]{ "head", "body", "tail" };
+            System.out.println(Arrays.deepToString(a));
+            System.out.println(Arrays.deepToString(fixTheMeerkat(a)));
+        }
+}
