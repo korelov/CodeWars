5912e6a3d8e679fcd6553c70a2100af5f1530e39
Korelov Maksim <korelov@gmail.com>
2021-09-29 22:11:13 +0300

    ou get an array of numbers, return the sum of all of the positives ones.

---
+++ src/Sum_of_positive.java
@@ -0,0 +1,31 @@
+//https://www.codewars.com/kata/5715eaedb436cf5606000381/train/java
+public class Sum_of_positive {
+    public static int sum(int[] arr) {
+
+        int result = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                result += arr[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        int[] a = new int[]{1, 2, 3, 4, 5};
+        System.out.println(sum(a));
+
+        int[] a1 = new int[]{1,-2,3,4,5};
+        System.out.println(sum(a1));
+
+        int[] a2 = new int[]{-1,-2,-3,-4,-5};
+        System.out.println(sum(a2));
+
+        int[] a4 = new int[]{};
+        System.out.println(sum(a4));
+
+
+    }
+
+}
