3be52f935ea05d25fded7446d72a63b51e666b3b
Korelov Maksim <korelov@gmail.com>
2021-10-26 06:40:26 +0300

    8kyu String charAt - 48

---
+++ src/Fake_Binary.java
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/57eae65a4321032ce000002d/train/java
+public class Fake_Binary {
+    public static String fakeBin(String numberString) {
+        String res = "";
+        for (int i = 0; i < numberString.length(); i++) {
+            if (numberString.charAt(i) - 48 < 5) {
+                res += "0";
+            } else {
+                res += "1";
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(fakeBin("45385593107843568"));
+        System.out.println(fakeBin("509321967506747"));
+        System.out.println(fakeBin("366058562030849490134388085"));
+    }
+}
