78a89a9ff819e969d52026cf8812ca3c01c1edd9
Maksim_Korelov <Korelov@gmail.com>
2021-10-27 14:41:30 +0300

    8 kyu Arrays

---
+++ src/N_th_Power.java
@@ -0,0 +1,22 @@
+//https://www.codewars.com/kata/57d814e4950d8489720008db/train/java
+public class N_th_Power {
+    public static int nthPower(int[] array, int n) {
+        int res = 0;
+        if(n > array.length-1){
+            res = -1;
+        }
+        for (int i = 0 ; i < array.length;i++){
+            if(i==n){
+                res = (int)Math.pow(array[i],n);
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nthPower(new int[] {1,2}, 2));
+        System.out.println(nthPower(new int[] {3,1,5,2}, 2));
+        System.out.println(nthPower(new int[] {3,1,2}, 2));
+    }
+}
+
