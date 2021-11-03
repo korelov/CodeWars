9a4d0b94c46521d2812fc2aab569b0c7247a5a0f
Maksim_Korelov <Korelov@gmail.com>
2021-11-03 19:23:44 +0300

    8 kyu String -> split()

---
+++ src/Triple_Trouble.java
@@ -0,0 +1,20 @@
+//https://www.codewars.com/kata/5704aea738428f4d30000914/train/java
+public class Triple_Trouble {
+    public static String tripleTrouble(String one, String two, String three) {
+
+        String[] arr1 = one.split("");
+        String[] arr2 = two.split("");
+        String[] arr3 = three.split("");
+        String res = "";
+        for (int i = 0; i < one.length(); i++) {
+            res += arr1[i] + arr2[i] + arr3[i];
+
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(tripleTrouble("this", "test", "lock"));
+        System.out.println(tripleTrouble("aa", "bb", "cc"));
+    }
+}
