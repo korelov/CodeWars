d42a335988e3e2e7dc26f66677e516229f7b58d1
Maksim Korelov <korelov@gmail.com>
2022-04-23 21:39:53 +0300

    6 kyu

---
+++ src/kyu_6/Break_camelCase.java
@@ -0,0 +1,29 @@
+package kyu_6;
+
+// https://www.codewars.com/kata/5208f99aee097e6552000148/train/java
+
+public class Break_camelCase {
+    public static String camelCase(String input) {
+        String res ="";
+        String temp =input.toLowerCase();
+
+        for (int i = 0; i < input.length(); i++) {
+            if(input.charAt(i)==temp.charAt(i)){
+                res+=input.charAt(i);
+            } else {
+                res+=" "+input.charAt(i);
+            }
+        }
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(camelCase("camelCasing"));
+        System.out.println(camelCase("camelCasingTest"));
+        System.out.println(camelCase("camelcasingtest"));
+
+    }
+
+}
