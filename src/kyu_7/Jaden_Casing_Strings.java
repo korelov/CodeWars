579d438e2863d841ee6547bdbee4e850cbdd2176
Maksim_Korelov <Korelov@gmail.com>
2022-05-07 17:41:08 +0300

    7 kyu

--- src/kyu_7/Jaden_Casing_Strings.java
+++ src/kyu_7/Jaden_Casing_Strings.java
@@ -2,25 +2,23 @@ package kyu_7;
 
 // https://www.codewars.com/kata/5390bac347d09b7da40006f6/train/java
 
-import java.util.Arrays;
-
 public class Jaden_Casing_Strings {
     public static String toJadenCase(String phrase) {
         if(phrase==null || phrase.length()==0){
             return null;
         }
-        String res = "";
+        StringBuilder res = new StringBuilder();
         for (int i = 0; i < phrase.length(); i++) {
             if (i == 0) {
-                res += phrase.toUpperCase().charAt(i);
+                res.append(phrase.toUpperCase().charAt(i));
             } else if (phrase.charAt(i) == ' ') {
-                res += " " + phrase.toUpperCase().charAt(i + 1);
+                res.append(" ").append(phrase.toUpperCase().charAt(i + 1));
                 i++;
             } else {
-                res += phrase.charAt(i);
+                res.append(phrase.charAt(i));
             }
         }
-        return res;
+        return res.toString();
     }
 
     public static void main(String[] args) {
