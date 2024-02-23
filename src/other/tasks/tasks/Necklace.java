6f9586985150aa06f0f0b98e612be64f7fb3d1f2
Korelov Maksim <korelov@gmail.com>
2024-02-23 17:14:50 +0300

    Necklace

---
+++ src/other/tasks/tasks/Necklace.java
@@ -0,0 +1,74 @@
+package other.tasks.tasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Necklace {
+
+    private static void checkNecklace(List<Integer> neck) {
+        if (neck.size() % 2 != 0) {
+            throw new IllegalArgumentException("Нечетное ожерелье");
+        }
+        List<Integer> humanOne = new ArrayList<>();
+        int humanOneDiamond = 0;
+        int humanOneEmerald = 0;
+        List<Integer> humanTwo = new ArrayList<>();
+        int humanTwoDiamond = 0;
+        int humanTwoEmerald = 0;
+
+        for (Integer stone : neck) {
+            if (stone == 0) {
+                if (humanOneDiamond == humanTwoDiamond) {
+                    humanOne.add(stone);
+                    humanOneDiamond++;
+                    continue;
+                }
+                if (humanOneDiamond > humanTwoDiamond) {
+                    humanTwo.add(stone);
+                    humanTwoDiamond++;
+                } else {
+                    humanOne.add(stone);
+                    humanOneDiamond++;
+                }
+            } else {
+                if (humanOneEmerald == humanTwoEmerald) {
+                    humanOne.add(stone);
+                    humanOneEmerald++;
+                    continue;
+                }
+                if (humanOneEmerald > humanTwoEmerald) {
+                    humanTwo.add(stone);
+                    humanTwoEmerald++;
+                } else {
+                    humanOne.add(stone);
+                    humanOneEmerald++;
+                }
+            }
+        }
+        if (humanOne.size() != humanTwo.size()) {
+            throw new IllegalArgumentException("Поделить поровну не получилось");
+        }
+        printResult(humanOne, humanOneDiamond, humanOneEmerald);
+        printResult(humanTwo, humanTwoDiamond, humanTwoEmerald);
+    }
+
+    private static void printResult(List<Integer> humanBag, int diamondCount, int emeraldCount) {
+        System.out.printf("У первого человека в сумке %d алмаза(ов), %d изумруда(ов), содержание сумки %s\n"
+                , diamondCount, emeraldCount, humanBag);
+    }
+
+    public static void main(String[] args) {
+//        проверка на нечетное ожерелье
+//        List<Integer> oddNecklace = List.of(0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0);
+//        checkNecklace(oddNecklace);
+
+        List<Integer> integerList = List.of(0, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0);
+        checkNecklace(integerList);
+
+        List<Integer> integerList1 = List.of(0, 0, 1, 1);
+        checkNecklace(integerList1);
+
+        List<Integer> integerList2 = List.of(0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1);
+        checkNecklace(integerList2);
+    }
+}
--- src/other/tasks/tasks/TenThousandDevils.java
+++ src/other/tasks/tasks/TenThousandDevils.java
@@ -1,7 +1,16 @@
 package other.tasks.tasks;
 
 public class TenThousandDevils {
+
+    private static void print(int number) {
+        if (number > 0) {
+            System.out.println("Учим Java, роняя слезы");
+            print(number - 1);
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Учим Java, роняя слезы\n".repeat(10_000));
+        // System.out.println("Учим Java, роняя слезы\n".repeat(10_000));
+        print(10_000);
     }
 }
